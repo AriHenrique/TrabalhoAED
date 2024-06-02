f4e882e57a97b0085d4d42f27e3710cf7ea95ce5
AriHenrique <arihenriquedev@hotmail.com>
2024-06-02 13:10:30 -0300

    ajuste quick

--- TrabalhoAED/Program.cs
+++ TrabalhoAED/Program.cs
@@ -13,11 +13,14 @@ namespace TrabalhoAED
             (int, int, Dictionary<int, Dictionary<string, int>>, List<Dictionary<string, string>>) dados =
                 EntradaDados.ProcessTxt("../../entrada.txt");
             
-            Console.WriteLine(JsonConvert.SerializeObject(dados.Item4, Formatting.Indented));
+            // Console.WriteLine(JsonConvert.SerializeObject(dados.Item4, Formatting.Indented));
             
             Console.WriteLine("------------------\n");
-            List<Dictionary<string, string>> test = Quick.QuicksortDesc(dados.Item4,0, dados.Item4.Count - 1, "redacao");
-            Console.WriteLine(JsonConvert.SerializeObject(test, Formatting.Indented));
+            List<Dictionary<string, string>> media = Quick.QuicksortDesc(dados.Item4,0, dados.Item4.Count - 1);
+            // List<Dictionary<string, string>> redacao = Quick.QuicksortDesc(media,0, dados.Item4.Count - 1, "redacao");
+            // List<Dictionary<string, string>> matematica = Quick.QuicksortDesc(redacao,0, dados.Item4.Count - 1, "matematica");
+            // List<Dictionary<string, string>> linguagens = Quick.QuicksortDesc(matematica,0, dados.Item4.Count - 1, "linguagens");
+            Console.WriteLine(JsonConvert.SerializeObject(media, Formatting.Indented));
         }
     }
 }
